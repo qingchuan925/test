aaebb1fca0c516ec4ece5d39a9ff467f1db19e28
qingchuan <406@tontisa.com>
2019-01-17 12:12:53 +0800

    init

---
+++ src/main/java/com/example/test/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: lyk
+ * @Date: 2019/1/17 12:04
+ * @Description:
+ */
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world;";
+    }
+}
